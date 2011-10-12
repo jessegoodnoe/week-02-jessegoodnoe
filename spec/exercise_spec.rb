e58e69c20bf80a7f212ec01623d41c639155f218
Jesse Goodnoe <jessegoodnoe@me.com>
2011-10-11 23:20:00 -0700

    Added another failing test.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,9 +2,18 @@ describe "Arrays" do
   it "creates a string from an array of ints" do
     int_string = ""
     [ 1, 2, 3, 4, 5 ].each do |i|
-      int_string << i.to_s
+      int_string << i
     end
     
     int_string.should == "12345"
+  end
+end
+
+
+describe "Integer" do
+  it "sums up 1 though 10" do
+    the_sum = nil
+    1.upto(10) { |i| the_sum += i }
+    the_sum.should eq 55
   end
 end
