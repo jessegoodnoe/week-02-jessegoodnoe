3aed7f7796d7dfa469fe0f38c36bd3a5b461a227
Tuefels Drock <uwjh0808@uw.edu>
2011-10-11 20:58:16 -0700

    fixed

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -2,7 +2,7 @@ describe "Arrays" do
   it "creates a string from an array of ints" do
     int_string = ""
     [ 1, 2, 3, 4, 5 ].each do |i|
-      int_string << i
+      int_string << i.to_s
     end
     
     int_string.should == "12345"
