3a630df11911f9d294b8292bab5d23baeb0678cd
Jesse Goodnoe <jessegoodnoe@me.com>
2011-10-11 20:33:53 -0700

    Failing test.

---
+++ README.md
@@ -0,0 +1,5 @@
+The test in here should create an string that looks like "12345".
+
+You might be interested in: 
+* http://rubydoc.info/stdlib/core/1.9.2/String
+* http://rubydoc.info/stdlib/core/1.9.2/Array
---
+++ spec/exercise_spec.rb
@@ -0,0 +1,10 @@
+describe "Arrays" do
+  it "creates a string from an array of ints" do
+    int_string = ""
+    [ 1, 2, 3, 4, 5 ].each do |i|
+      int_string << i
+    end
+    
+    int_string.should == "12345"
+  end
+end
